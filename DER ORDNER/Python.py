fb460056ea40418e550e333c4ea13a8d29b5dbae
Ruffy93 <rafael-neumann@web.de>
2025-05-22 11:13:57 +0200

    Hier Samu

---
+++ DER ORDNER/Python.py
@@ -0,0 +1,17 @@
+import random
+import time
+
+Trommel = [False, False, False, False, False, True]
+random.shuffle(Trommel)
+
+print("🔫 Komm, lass uns Russisch Roulette spielen ;D")
+time.sleep(1)
+input("Drücke [Enter], um den Abzug zu betätigen, Leben oder Tod?")
+
+Trommel_index = random.randint(0, 5)
+
+if Trommel[Trommel_index]:
+    print("💥 Boom! Du bist gestorben.")
+else:
+    print("💨 Klick... Du hast Schwein gehabt.")
+
